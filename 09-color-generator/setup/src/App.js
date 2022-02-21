74aa56fa138b6530db59f48e37f63894a228888b
Kaushik Chemburkar <attherate@gmail.com>
2022-02-21 17:23:17 +1100

    displaying hex colours

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -13,7 +13,8 @@ function App() {
     // console.log('hello')
     try {
       let colors = new Values(color).all(10)
-      console.log(colors)
+      // console.log(colors)
+      setList(colors)
     } catch (error) {
       setError(true)
       console.log(error)
@@ -38,7 +39,17 @@ function App() {
       </section>
 
       <section className='colors'>
-        <h4>list goes here</h4>
+        {list.map((color, index) => {
+          // console.log(color)
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          )
+        })}
       </section>
     </>
   )
