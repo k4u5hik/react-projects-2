815123e0e9f73b8dfe35ccdb5a3805c678235c85
Kaushik Chemburkar <attherate@gmail.com>
2022-02-21 11:35:14 +1100

    Basic generator setup

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,35 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from 'react'
+import data from './data'
 function App() {
-  
-  
+  const [count, setCount] = useState(0)
+  const [text, setText] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('hello world')
+  }
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className='section-center'>
+      <h3> lorem ipsum ?</h3>
+      <form action='lorem-form' onSubmit={handleSubmit}>
+        <label htmlFor='amount'>paragraphs</label>
+        <input
+          type='number'
+          name='amount'
+          id='amount'
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type='submit' className='btn'>
+          generate
+        </button>
+      </form>
+      <article className='lorem-text'>
+        <p>Lorem iprsum 1</p>
+        <p>Lorem iprsum 2</p>
+      </article>
+    </section>
+  )
 }
 
-export default App;
+export default App
