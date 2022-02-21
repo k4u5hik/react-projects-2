b3ca09d869648594d1a4ee0d146781f8792740aa
Kaushik Chemburkar <attherate@gmail.com>
2022-02-22 09:53:50 +1100

    inspect element > components and console to see if everything is working

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -5,7 +5,7 @@ import Alert from './Alert'
 function App() {
   const [name, setName] = useState('')
   const [list, setList] = useState([])
-  const [isEditing, setIsEditing] = useState(true)
+  const [isEditing, setIsEditing] = useState(false)
   const [editID, setEditID] = useState(null)
   const [alert, setAlert] = useState({ show: false, msg: '', type: '' })
   const handleSubmit = (e) => {
@@ -19,7 +19,13 @@ function App() {
         {alert.show && <Alert />}
         <h3>grocery bud</h3>
         <div className='form-control'>
-          <input type='' />
+          <input
+            type=''
+            className='grocery'
+            placeholder='eg: eggs'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
           <button type='submit' className='submit-btn'>
             {isEditing ? 'edit' : 'submit'}
           </button>
