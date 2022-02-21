42c4a39ae732a41350ef39ddd692e5a178821fbe
Kaushik Chemburkar <attherate@gmail.com>
2022-02-21 16:39:26 +1100

    Error: border turns red if wrong colour value is entered

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -10,7 +10,14 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log('hello')
+    // console.log('hello')
+    try {
+      let colors = new Values(color).all(10)
+      console.log(colors)
+    } catch (error) {
+      setError(true)
+      console.log(error)
+    }
   }
   return (
     <>
@@ -22,6 +29,7 @@ function App() {
             value={color}
             onChange={(e) => setColor(e.target.value)}
             placeholder='#f15025'
+            className={`${error ? 'error' : null}`}
           />
           <button className='btn' type='submit'>
             Submit
