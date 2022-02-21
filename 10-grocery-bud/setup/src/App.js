64ca3ad73d21015ebb1aa9f11fffdc15a69a5d5a
Kaushik Chemburkar <attherate@gmail.com>
2022-02-22 10:47:43 +1100

    displaying the items in the list after entering text and pressing submit

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -10,7 +10,17 @@ function App() {
   const [alert, setAlert] = useState({ show: false, msg: '', type: '' })
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log('hello')
+    // console.log('hello')
+    if (!name) {
+      //display alert
+    } else if (name && isEditing) {
+      //deal with edit
+    } else {
+      // show alert
+      const newItem = { id: new Date().getTime().toString(), title: name }
+      setList([...list, newItem])
+      setName('')
+    }
   }
 
   return (
@@ -33,7 +43,7 @@ function App() {
       </form>
 
       <div className='grocery-container'>
-        <List />
+        <List items={list} />
         <button className='clear-btn'>clear items</button>
       </div>
     </section>
