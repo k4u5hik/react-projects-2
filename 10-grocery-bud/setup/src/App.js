32118dec2875cd0ed4f41b2a08396a41f5342095
Kaushik Chemburkar <attherate@gmail.com>
2022-02-22 17:37:02 +1100

    remove individual item

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -36,6 +36,12 @@ function App() {
     showAlert(true, 'danger', 'empty list')
     setList([])
   }
+
+  const removeItem = (id) => {
+    showAlert(true, 'danger', 'Item removed')
+    setList(list.filter((item) => item.id !== id))
+  }
+
   return (
     <section className='section-center'>
       <form className='grocery-form' onSubmit={handleSubmit}>
@@ -56,7 +62,7 @@ function App() {
       </form>
       {list.length > 0 && (
         <div className='grocery-container'>
-          <List items={list} />
+          <List items={list} removeItem={removeItem} />
           <button className='clear-btn' onClick={clearList}>
             clear items
           </button>
