6445bfb28a48c25317746ba7c7a93b13be6fff57
Kaushik Chemburkar <attherate@gmail.com>
2022-02-23 13:57:59 +1100

    data from data.js used in navbar instead of hardcoding values

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -15,36 +15,25 @@ const Navbar = () => {
         </div>
         <div className='links-container show-container'>
           <ul className='links'>
-            <li>
-              <a href='#'>Home</a>
-            </li>
-            <li>
-              <a href='#'>About</a>
-            </li>
-            <li>
-              <a href='#'>Contact</a>
-            </li>
-            <li>
-              <a href='#'>Products</a>
-            </li>
+            {links.map((link) => {
+              const { id, url, text } = link
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              )
+            })}
           </ul>
         </div>
         <ul className='social-icons'>
-          <li>
-            <a href='https://www.abc.com'>
-              <FaTwitter />
-            </a>
-          </li>
-          <li>
-            <a href='https://www.abc.com'>
-              <FaInstagram />
-            </a>
-          </li>
-          <li>
-            <a href='https://www.abc.com'>
-              <FaReddit />
-            </a>
-          </li>
+          {social.map((socialIcon) => {
+            const { id, url, icon } = socialIcon
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            )
+          })}
         </ul>
       </div>
     </nav>
