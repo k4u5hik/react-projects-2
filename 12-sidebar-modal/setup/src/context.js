4da594bcc1f81b282ee11f09069038efc2fa8ede
Kaushik Chemburkar <attherate@gmail.com>
2022-03-03 11:35:44 +1100

    AppContext console log

--- 12-sidebar-modal/setup/src/context.js
+++ 12-sidebar-modal/setup/src/context.js
@@ -1,1 +1,13 @@
 import React, { useState, useContext } from 'react'
+
+const AppContext = React.createContext()
+
+const AppProvider = ({ children }) => {
+  return (
+    <AppContext.Provider value='hello app context'>
+      {children}
+    </AppContext.Provider>
+  )
+}
+
+export { AppContext, AppProvider }
