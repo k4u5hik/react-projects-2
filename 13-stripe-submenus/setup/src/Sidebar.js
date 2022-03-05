272a7c1ca983099220c3d8e567af7f86d8afbea8
Kaushik Chemburkar <attherate@gmail.com>
2022-03-05 23:17:16 +1100

    background got darker

--- 13-stripe-submenus/setup/src/Sidebar.js
+++ 13-stripe-submenus/setup/src/Sidebar.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import { FaTimes } from 'react-icons/fa'
 import sublinks from './data'
+import { useGlobalContext } from './context'
 
 const Sidebar = () => {
-  return <h2>sidebar component</h2>
+  const { isSidebarOpen, closeSidebar } = useGlobalContext()
+  return (
+    <aside
+      className={`${
+        isSidebarOpen ? 'sidebar-wrapper show' : 'sidebar-wrapper'
+      }`}
+    >
+      sidebar component
+    </aside>
+  )
 }
 
 export default Sidebar
