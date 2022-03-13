e186d96058d4d876c13dc3dbe28759f63c31b205
Kaushik Chemburkar <attherate@gmail.com>
2022-03-14 10:16:59 +1100

    Increase and decrease cartItems quantity

--- 14-cart/setup/src/reducer.js
+++ 14-cart/setup/src/reducer.js
@@ -11,6 +11,35 @@ const reducer = (state, action) => {
       cart: state.cart.filter((cartItem) => cartItem.id !== action.payload),
     }
   }
+  if (action.type === 'INCREASE') {
+    return {
+      ...state,
+      cart: state.cart.map((cartItem) => {
+        if (cartItem.id === action.payload) {
+          return {
+            ...cartItem,
+            amount: cartItem.amount + 1,
+          }
+        }
+        return cartItem
+      }),
+    }
+  }
+  if (action.type === 'DECREASE') {
+    return {
+      ...state,
+      cart: state.cart.map((cartItem) => {
+        if (cartItem.id === action.payload) {
+          return {
+            ...cartItem,
+            amount: cartItem.amount - 1,
+          }
+        }
+        return cartItem
+      }),
+    }
+  }
+
   return state
 }
 
