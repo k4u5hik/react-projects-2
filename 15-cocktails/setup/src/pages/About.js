0c300b1976fb9bbcc95cc0cdfff1106ab9573bb5
Kaushik Chemburkar <attherate@gmail.com>
2022-03-16 08:55:31 +1100

    Added structure to the About and Error page

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -2,9 +2,25 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
+        consectetur, quos temporibus officiis vitae a molestias vel iusto
+        voluptates ipsam. Repudiandae, perferendis enim reiciendis et est fugiat
+        quidem rem fuga. Cum a ad, possimus perferendis maxime officia officiis
+        minima dolorem earum pariatur minus doloribus sunt ipsum, nemo labore
+        beatae in illum quae et accusamus enim. Minus blanditiis nam fugit
+        mollitia? Sint maxime consectetur, nostrum ratione obcaecati laudantium
+        ullam magnam exercitationem voluptatum voluptas reiciendis aliquid quae
+        ipsam fuga quidem molestiae debitis. Iste nam qui modi esse sint, sequi
+        distinctio assumenda error? Nisi saepe doloremque necessitatibus
+        praesentium voluptatem. Pariatur odit adipisci quas. Culpa dolore iste
+        nam delectus, perspiciatis inventore, eveniet consequatur itaque
+        pariatur tempore, necessitatibus in quo reprehenderit molestias
+        repudiandae aperiam a.
+      </p>
+    </section>
   )
 }
 
