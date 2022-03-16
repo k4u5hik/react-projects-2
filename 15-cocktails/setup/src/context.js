6199689a226ef30544a9442fb072b6a2029c19d2
Kaushik Chemburkar <attherate@gmail.com>
2022-03-16 16:48:32 +1100

    removed searchTerm and loading back to true

--- 15-cocktails/setup/src/context.js
+++ 15-cocktails/setup/src/context.js
@@ -5,14 +5,13 @@ const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(true)
   const [searchTerm, setSearchTerm] = useState('a')
   const [cocktails, setCocktails] = useState([])
   return (
     <AppContext.Provider
       value={{
         loading,
-        searchTerm,
         cocktails,
         setSearchTerm,
       }}
