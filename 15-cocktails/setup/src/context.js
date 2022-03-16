d9b348b59bbbd89e10283ff72e031cdfe143798f
Kaushik Chemburkar <attherate@gmail.com>
2022-03-16 17:15:59 +1100

    Console logs - we get console logged object with drinks property which is an array

--- 15-cocktails/setup/src/context.js
+++ 15-cocktails/setup/src/context.js
@@ -8,6 +8,23 @@ const AppProvider = ({ children }) => {
   const [loading, setLoading] = useState(true)
   const [searchTerm, setSearchTerm] = useState('a')
   const [cocktails, setCocktails] = useState([])
+
+  const fetchDrinks = async () => {
+    setLoading(true)
+    try {
+      const response = await fetch(`${url}${searchTerm}`)
+      const data = await response.json()
+      console.log(data)
+      setCocktails(data)
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  useEffect(() => {
+    fetchDrinks()
+  }, [searchTerm])
+
   return (
     <AppContext.Provider
       value={{
