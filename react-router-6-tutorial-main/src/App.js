275605c063d17db6fe0a1af9d25abee8531af36b
Kaushik Chemburkar <attherate@gmail.com>
2022-03-19 18:36:35 +1100

    Basic routing on App.js

--- react-router-6-tutorial-main/src/App.js
+++ react-router-6-tutorial-main/src/App.js
@@ -1,5 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 function App() {
-  return <h2>React Router 6 Tutorial</h2>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<h1>Home</h1>} />
+        <Route path='/about' element={<h1>About</h1>} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
