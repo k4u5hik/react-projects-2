077675a7cf7eaf4d901bf7f455aa2c4b37e0b186
Kaushik Chemburkar <attherate@gmail.com>
2022-03-19 18:40:31 +1100

    pasted App.js from readme

--- react-router-6-tutorial-main/src/App.js
+++ react-router-6-tutorial-main/src/App.js
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Products from './pages/Products'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<h1>Home</h1>} />
-        <Route path='/about' element={<h1>About</h1>} />
+        <Route path='/' element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='products' element={<Products />} />
       </Routes>
     </BrowserRouter>
   )
