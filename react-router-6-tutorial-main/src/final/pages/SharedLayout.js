433f6387855894bc5ab213131aa1cfcc599cd30e
Kaushik Chemburkar <attherate@gmail.com>
2022-03-22 08:22:26 +1100

    changed from h1 to h2

--- react-router-6-tutorial-main/src/final/pages/SharedLayout.js
+++ react-router-6-tutorial-main/src/final/pages/SharedLayout.js
@@ -1,12 +1,11 @@
 import { Link, Outlet } from 'react-router-dom'
 import Navbar from '../components/Navbar'
+import StyledNavbar from '../components/StyledNavbar'
 const SharedLayout = () => {
   return (
     <>
-      <Navbar />
-      <section className='section'>
-        <Outlet />
-      </section>
+      <StyledNavbar />
+      <Outlet />
     </>
   )
 }
